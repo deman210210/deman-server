30952e2e07d764518ddb08b3fb8298e2bbcf83e9
deman210210 <kalshly851@gmail.com>
2025-08-04 04:37:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "✅ السيرفر شغال!"
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    data = request.get_json()
+    print("📩 وصلك أمر:", data)
+    return "تم الاستلام"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
